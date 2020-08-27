6efa8e6d3a27a97a3ef4a87aba89d4afa1f9e4a8
CaptainSemicolon <steve,gagne@hotmail.com>
2020-08-27 17:09:42 -0400

    Step 0

--- js/birthday.js
+++ js/birthday.js
@@ -1,4 +1,5 @@
 var hints = [
+    {id:0,title:"Step 00: Project Birthday",text:"Hello Audrey!<br>What follows will be what I like to call Project Birthday.<br>It took a while to set up and hopefully it will be as enjoyable to play as it was to create.<br>Without anymore waiting, enjoy the game.",image:"../images/balloon-background.jpg"},
     {id:1,title:"Step 01: Introduction",text:"Hello everyone!<br>Right now in Vaudreuil it is " + moment().format('h:mm a') + ".<br>Looks like we have a special announcement! Happy birthday Audrey!<br>To celebrate, some villagers have got you gifts... although they hid them. Ah! Perhaps a bit of treasure hunting... Let's make sure you are HOME before we begin. First, click the localization button and the next step should light up.",image:"../images/isabellemain.jpg"},
     {id:2,title:"Step 02: Introduction",text:"Good! It all works!<br>Nook phones aren't always reliable...<br>Anyway! Like I was saying, some villagers got you gifts but hid them, let's see if you can find them. Click the 'Next Step' button when you are ready to find the first gift.",image:"../images/isabellemain.jpg"},
     {id:3,title:"Step 03: Bunnie's Gift",text:"Happy birthday Audrey!<br>I got you a great gift, but you'll have to work a bit to find out where I put it. I have a clue for you, tap my picture to reveal it!<br>Once you know where to go just head there, the next step will unlock once you know the right place!",image:"../images/NH-Bunnie_poster.png"},
@@ -37,16 +38,16 @@ $( document ).ready(function() {
 
 function initializePage(){
     var page = parseInt((window.location.pathname).match(/(\d+)/)[0]);
-    $("#clue-title").html(hints[page-1].title);
-    $("#clue-picture").attr("src", hints[page-1].image);
-    typewriter(hints[page-1].text);
+    $("#clue-title").html(hints[page].title);
+    $("#clue-picture").attr("src", hints[page].image);
+    typewriter(hints[page].text);
 }
 
 async function typewriter(message){
     for(var i = 0; i != message.length + 1; i++){
         if(message.charAt(i) != '<' && message.charAt(i+1) != 'b' && message.charAt(i+2) != 'r' && message.charAt(i+3) != '>'){
             $('#clue-text').html(message.substring(0, i));
-            await sleep(0);
+            await sleep(50);
         }
         else
             i = i+1;
